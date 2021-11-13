1d1689b1028197842b6361ece9fa78d5b44b02b2
Nura Tamton <nrt2000@hw.ac.uk>
2021-11-13 14:40:31 +0400

    added ejs files

--- lab02_v1/app.js
+++ lab02_v1/app.js
@@ -1,13 +1,30 @@
 const express = require ('express');
 
+
 //creating app
 const app = express();
+
 
 //send an HTTP response when receiving HTTP GET/
 
 app.use(express.static('public'));
+/*
 app.get('/', (req,res) => {
-    res.sendFile('public/index.html', {root:__dirname});
+    res.sendFile('index.html', {root:__dirname});
+});
+*/
+
+//handling static HTML and EJS templates
+app.use(express.static('public'));
+app.set('view engine','ejs');
+app.get('/',(req,res)=>{
+    res.render('index'); //no need for ejs extension
+});
+
+
+//route for contacts
+app.get('/contacts',(req,res)=> {
+    res.render('contacts');
 });
 
 
