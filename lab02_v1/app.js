0b0138d2d4ff6eef190ede00f67303178e3f8de5
Nura Tamton <nrt2000@hw.ac.uk>
2021-11-18 18:09:17 +0400

    added db, controllers and services

--- lab02_v1/app.js
+++ lab02_v1/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 //send an HTTP response when receiving HTTP GET/
 
-app.use(express.static('public'));
+//app.use(express.static('public'));
 /*
 app.get('/', (req,res) => {
     res.sendFile('index.html', {root:__dirname});
@@ -16,7 +16,9 @@ app.get('/', (req,res) => {
 
 //handling static HTML and EJS templates
 app.use(express.static('public'));
-app.set('view engine','ejs');
+app.set('view engine','ejs'); //to set up ejs
+
+//Sending static files as an http response to http GET
 app.get('/',(req,res)=>{
     res.render('index'); //no need for ejs extension
 });
@@ -27,9 +29,29 @@ app.get('/contacts',(req,res)=> {
     res.render('contacts');
 });
 
+//route for login
+app.get('/login', (req, res) => { res.render('login');
+});
+
+//route for register
+app.get('/register', (req, res) => { res.render('register');
+});
+
+//route for catalogue 
+app.get('/catalogue',(req,res)=> {res.render('catalogue');
+});
+
+
+// using JSON and URL Encoded middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //make the app listen on port
 const port = process.argv[2] || process.env.PORT || 3000;
 const server = app.listen(port,()=> {
     console.log('Cart app listening at http://localhost:${port}');
 })
+
+//pass requests to the router middleware
+const router = require('./routes/apis'); 
+app.use(router);
